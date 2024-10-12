c3bf30219ec9d4f0ba4a80eb6f15875b8ec5eea1
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-12 07:27:23 -0500

    Improve looks and functionality.

--- RPGCoolGuy/Controllers/HomeController.cs
+++ RPGCoolGuy/Controllers/HomeController.cs
@@ -13,21 +13,24 @@ namespace RPGCoolGuy.Controllers
             ctx = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            var chars = ctx.Characters.ToList();
+            var chars = ctx.Characters.OrderBy(c => c.Name).ToList();
             return View(chars);
         }
 
         [HttpGet]
         public IActionResult Add()
         {
+            ViewData["Title"] = "Add Character";
             return View("CharacterForm");
         }
 
         [HttpGet]
         public IActionResult Edit(int id)
         {
+            ViewData["Title"] = "Edit Character";
             var c = ctx.Characters.Find(id);
             return View("CharacterForm", c);
         }
@@ -49,16 +52,24 @@ namespace RPGCoolGuy.Controllers
                 var chars = ctx.Characters.ToList();
                 return View("Index", chars);
             }
-            else { return View("CharacterForm", c); }
+            else
+            {
+                return View("CharacterForm", c);
+            }
         }
 
+        [HttpPost]
         public IActionResult Delete(int id)
         {
-            Character del = ctx.Characters.Find(id);
-            ctx.Characters.Remove(del);
-            ctx.SaveChanges();
-            var c = ctx.Characters.ToList();
-            return View("Index", c);
+            Character? character = ctx.Characters.Find(id);
+
+            if (character != null)
+            {
+                ctx.Characters.Remove(character);
+                ctx.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
